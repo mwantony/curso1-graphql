c24aea828dc17ed068112f506fdee2131ab90cd2
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-22 13:48:43 -0300

    cap 4 vid 1 finalizado

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -1,14 +1,20 @@
 import { api, opcoesFetch } from "./config";
 
 const listarClientes = () =>
-  fetch(`http://localhost:4000`, opcoesFetch("{clientes {nome cpf}}"))
+  fetch(`http://localhost:4000`, opcoesFetch("{clientes {id nome cpf}}"))
     .then((resposta) => resposta.json())
     .then((dados) => {
       return dados.data.clientes;
     });
 const buscarClientePorId = (id) =>
-  api.get(`/clientes/cliente/${id}`).then((resposta) => resposta.data[0]);
-
+    fetch('http://localhost:4000', opcoesFetch(`
+      {
+        cliente(id: ${id}) {
+          nome cpf
+        }
+      }
+    `)).then(resposta => resposta.json())
+    .then(dados => dados.data.cliente)
 const adicionarCliente = (cliente) =>
   fetch(
     `http://localhost:4000`,
@@ -21,7 +27,14 @@ const adicionarCliente = (cliente) =>
   })
 
 const alterarCliente = (id, cliente) =>
-  api.put(`/clientes/cliente/${id}`, cliente).then((resposta) => resposta.data);
+  fetch('http://localhost:4000', opcoesFetch(`
+    mutation {
+      atualizarCliente(id: ${id}, nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+        nome
+      }
+    }
+  `)).then(resposta => resposta.json())
+  .then(dados => dados.data)
 
 const removerCliente = (id) =>
   api.delete(`/clientes/cliente/${id}`).then((resposta) => resposta.data);
