3f7a760a1ed95eb432da57d52856618b877dfac0
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-22 13:00:21 -0300

    cap 2 vid 2 finalizado

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -1,34 +1,32 @@
-import { api } from './config'
+import { api } from "./config";
 
-const listarClientes = () => 
-  api
-    .get('/clientes')
-    .then(resposta => resposta.data)
+const listarClientes = () =>
+  fetch(`http://localhost:4000`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: "{clientes {nome cpf}}" }),
+  }).then(resposta => resposta.json()).then(dados => {
+    console.log(dados)
+    return []
+  })
+const buscarClientePorId = (id) =>
+  api.get(`/clientes/cliente/${id}`).then((resposta) => resposta.data[0]);
 
-const buscarClientePorId = id => 
-  api
-    .get(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data[0])
-
-const adicionarCliente = cliente => 
-  api
-    .post('/clientes/cliente', cliente)
-    .then(resposta => resposta.data)
+const adicionarCliente = (cliente) =>
+  api.post("/clientes/cliente", cliente).then((resposta) => resposta.data);
 
 const alterarCliente = (id, cliente) =>
-  api
-    .put(`/clientes/cliente/${id}`, cliente)
-    .then(resposta => resposta.data)
+  api.put(`/clientes/cliente/${id}`, cliente).then((resposta) => resposta.data);
 
-const removerCliente = id => 
-  api
-    .delete(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data)
+const removerCliente = (id) =>
+  api.delete(`/clientes/cliente/${id}`).then((resposta) => resposta.data);
 
 export default {
   listarClientes,
   buscarClientePorId,
   adicionarCliente,
   alterarCliente,
-  removerCliente
-}
+  removerCliente,
+};
