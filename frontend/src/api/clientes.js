5c54272a54954cc1fa32a21b6b1ce3b061eed507
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-22 13:34:00 -0300

    cap 3 finalizado

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -1,18 +1,24 @@
 import { api, opcoesFetch } from "./config";
 
-
 const listarClientes = () =>
   fetch(`http://localhost:4000`, opcoesFetch("{clientes {nome cpf}}"))
     .then((resposta) => resposta.json())
     .then((dados) => {
-      console.log(dados);
       return dados.data.clientes;
     });
 const buscarClientePorId = (id) =>
   api.get(`/clientes/cliente/${id}`).then((resposta) => resposta.data[0]);
 
 const adicionarCliente = (cliente) =>
-  api.post("/clientes/cliente", cliente).then((resposta) => resposta.data);
+  fetch(
+    `http://localhost:4000`,
+    opcoesFetch(
+      `mutation {adicionarCliente(nome: "${cliente.nome}", cpf: "${cliente.cpf}") {nome cpf}}`
+    )
+  ).then((resposta) => resposta.json())
+  .then((dados) => {
+    return dados.data.adicionarCliente;
+  })
 
 const alterarCliente = (id, cliente) =>
   api.put(`/clientes/cliente/${id}`, cliente).then((resposta) => resposta.data);
