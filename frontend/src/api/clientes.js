84b30b9c07023daaa80bc1a2a758a397c4663d66
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-25 12:20:38 -0300

    open

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -1,50 +1,59 @@
-import { api, opcoesFetch } from './config'
+import { api, opcoesFetch } from "./config";
 
 const listarClientes = () =>
-  fetch('http://localhost:4000/', opcoesFetch('{clientes { id nome cpf }}'))
-    .then(resposta => resposta.json())
-    .then(dados => dados.data.clientes)
+  fetch("http://localhost:4000/", opcoesFetch("{clientes { id nome cpf }}"))
+    .then((resposta) => resposta.json())
+    .then((dados) => dados.data.clientes);
 
-const buscarClientePorId = id =>
-  fetch('http://localhost:4000', opcoesFetch(`{
+const buscarClientePorId = (id) =>
+  fetch(
+    "http://localhost:4000",
+    opcoesFetch(`{
     cliente(id: ${id}) {
       nome
       cpf
     }
-  }`))
-    .then(resposta => resposta.json())
-    .then(dados => dados.data.cliente)
+  }`)
+  )
+    .then((resposta) => resposta.json())
+    .then(() => []);
 
-const adicionarCliente = cliente =>
-  fetch('http://localhost:4000', opcoesFetch(`
+const adicionarCliente = (cliente) =>
+  fetch(
+    "http://localhost:4000",
+    opcoesFetch(`
     mutation {
       adicionarCliente(nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
         id
         nome
       }
     }
-  `))
-    .then(resposta => resposta.json())
-    .then(dados => dados.data.cliente)
+  `)
+  )
+    .then((resposta) => resposta.json())
+    .then((dados) => dados.data.cliente);
 
 const alterarCliente = (id, cliente) =>
-  fetch('http://localhost:4000', opcoesFetch(`
+  fetch(
+    "http://localhost:4000",
+    opcoesFetch(`
     mutation {
       atualizarCliente(id: ${id}, nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
         nome
       }
     }
-  `))
-    .then(resposta => resposta.json())
-    .then(dados => dados.data)
+  `)
+  )
+    .then((resposta) => resposta.json())
+    .then((dados) => dados.data);
 
-const removerCliente = id =>
-  api.delete(`/clientes/cliente/${id}`).then(resposta => resposta.data)
+const removerCliente = (id) =>
+  api.delete(`/clientes/cliente/${id}`).then((resposta) => resposta.data);
 
 export default {
   listarClientes,
   buscarClientePorId,
   adicionarCliente,
   alterarCliente,
-  removerCliente
-}
+  removerCliente,
+};
